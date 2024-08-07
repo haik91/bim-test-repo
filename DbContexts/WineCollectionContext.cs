5833c1b1e9aea34fb2ababebcf04de43de2f2d4e
haik91 <hadi33806@gmail.com>
2024-08-07 12:29:53 +0200

    add libraries enity framework core, tools , and sql server. and configured the dbcontext for the database

---
+++ DbContexts/WineCollectionContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WinemakerAPI.Models;
+
+namespace WinemakerAPI.DbContexts
+{
+    public class WineCollectionContext : DbContext
+    {
+        public WineCollectionContext(DbContextOptions<WineCollectionContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Winemaker> Winemakers { get; set; }
+        public DbSet<WineBottle> WineBottles { get; set; }
+    }
+}
