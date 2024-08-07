0e8ee94f5c31bbc7c0f21ba1c624f5f57af3bf0d
haik91 <hadi33806@gmail.com>
2024-08-07 16:29:07 +0200

    add mapping proile

---
+++ MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using WinemakerAPI.Entities;
+using WinemakerAPI.Models;
+
+namespace WinemakerAPI
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // winemakers mappings
+            CreateMap<PostWineMaker, WineMaker>();
+            CreateMap<WineMaker, GetWineMaker>();
+        }
+    }
+}
