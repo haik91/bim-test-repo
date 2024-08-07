93157844ba7e6b5f430ad73623a4baf2bcd68dd5
haik91 <hadi33806@gmail.com>
2024-08-07 16:32:45 +0200

    add dtos for wine maker controller

---
+++ Models/GetWineMaker.cs
@@ -0,0 +1,12 @@
+﻿using WinemakerAPI.Entities;
+
+namespace WinemakerAPI.Models
+{
+    public class GetWineMaker
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public ICollection<GetWineBottle> WineBottles { get; set; }
+    }
+}
---
+++ Models/PostWinemaker.cs
@@ -0,0 +1,8 @@
+﻿namespace WinemakerAPI.Models
+{
+    public class PostWineMaker
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
