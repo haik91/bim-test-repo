b2f2feb397dc0ff6a5f97b1f31446c1d503342e1
haik91 <hadi33806@gmail.com>
2024-08-07 16:14:59 +0200

    add comments to the Wine maker entity

--- Entities/WineMaker.cs
+++ Entities/WineMaker.cs
@@ -1,10 +1,28 @@
 ﻿namespace WinemakerAPI.Entities
 {
+    /// <summary>
+    /// Represents a wine maker entity.
+    /// </summary>
     public class WineMaker
     {
+        /// <summary>
+        /// Thehe unique identifier for the wine maker.
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// The name of the wine maker.
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// The address of the wine maker.
+        /// </summary>
         public string Address { get; set; }
+
+        /// <summary>
+        /// The collection of wine bottles associated with the wine maker.
+        /// </summary>
         public ICollection<WineBottle> WineBottles { get; set; }
     }
 }
