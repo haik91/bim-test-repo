2a3ac082c98f650b518a36bba347feb84be9e7ae
haik91 <hadi33806@gmail.com>
2024-08-07 15:59:49 +0200

    add automapper and included swagger xml file to be able to leave comments in the classes

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using WinemakerAPI.DbContexts;
+using WinemakerAPI;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,10 +9,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<WineCollectionContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory,
+        $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"));
+});
 
 var app = builder.Build();
 
