81f076a2697366d8720fa6b7518d9ee27ddc8f14
haik91 <hadi33806@gmail.com>
2024-08-08 08:45:34 +0200

    add data annotation to wineBottle entity and post wine bottle request dto

--- Entities/WineBottle.cs
+++ Entities/WineBottle.cs
@@ -1,4 +1,6 @@
-﻿namespace WinemakerAPI.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WinemakerAPI.Entities
 {
     /// <summary>
     /// Represents a wine bottle.
@@ -8,61 +10,74 @@
         /// <summary>
         /// The unique identifier for the wine bottle.
         /// </summary>
+        [Key]
         public int Id { get; set; }
 
         /// <summary>
         /// The name of the wine bottle.
         /// </summary>
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
 
         /// <summary>
         /// The year the wine was produced.
         /// </summary>
+        [Range(1900, 2024)]
         public int Year { get; set; }
 
         /// <summary>
         /// The size of the wine bottle in milliliters (ml).
         /// </summary>
-        public int Size { get; set; } 
+        [Range(50, 2000)]
+        public int Size { get; set; }
 
         /// <summary>
         /// The number of bottles available in the wine cellar.
         /// </summary>
+        [Range(0, 3000)]
         public int CountInWineCellar { get; set; }
 
         /// <summary>
         /// The style of the wine.
         /// </summary>
+        [StringLength(50)]
         public string Style { get; set; }
 
         /// <summary>
         /// A description of the wine's taste.
         /// </summary>
+        [StringLength(500)]
         public string Taste { get; set; }
 
         /// <summary>
         /// A detailed description of the wine.
         /// </summary>
+        [StringLength(1000)]
         public string Description { get; set; }
 
         /// <summary>
         /// Food pairings that go well with the wine.
         /// </summary>
+        [StringLength(500)]
         public string FoodPairing { get; set; }
 
         /// <summary>
         /// A link to more information about the wine.
         /// </summary>
+        [Url]
         public string Link { get; set; }
 
         /// <summary>
         /// The URL of the wine bottle's image.
         /// </summary>
+        [Url]
         public string Image { get; set; }
 
         /// <summary>
         /// The unique identifier of the associated wine maker.
         /// </summary>
+        [Required]
         public int WineMakerId { get; set; }
 
         /// <summary>
--- Models/PostWineBottle.cs
+++ Models/PostWineBottle.cs
@@ -1,17 +1,41 @@
-﻿namespace WinemakerAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WinemakerAPI.Models
 {
     public class PostWineBottle
     {
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
+
+        [Range(1900, 2024)]
         public int Year { get; set; }
+
+        [Range(50, 2000)]
         public int Size { get; set; } // Size in ml
+
+        [Range(0, 3000)]
         public int CountInWineCellar { get; set; }
+
+        [StringLength(50)]
         public string Style { get; set; }
+
+        [StringLength(500)]
         public string Taste { get; set; } // Text description of taste
+
+        [StringLength(1000)]
         public string Description { get; set; }
+
+        [StringLength(500)]
         public string FoodPairing { get; set; }
+
+        [Url]
         public string Link { get; set; }
+
+        [Url]
         public string Image { get; set; } // URL to image
+
+        [Required]
         public int WineMakerId { get; set; }
     }
 }
